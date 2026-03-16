7f487373214ad8619c84aa1eb0adb2d7626f5848
Gabriel Freitas <ggfto@outlook.com>
2026-03-16 13:09:21 -0300

    fix: lint

--- client/wall.lua
+++ client/wall.lua
@@ -68,7 +68,6 @@ end)
 
 function DrawText3D(x,y,z, text, r,g,b)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
     if onScreen then
         SetTextFont(4)
         SetTextProportional(1)
