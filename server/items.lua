152bd4053c562aecc9af1fc5c9f60e8cc5fa99ec
Gabriel Freitas <ggfto@outlook.com>
2026-04-13 12:30:01 -0300

    feat: melhorias no processamento de itens

--- server/items.lua
+++ server/items.lua
@@ -1,24 +1,37 @@
 local function GetItemsList()
     local Items = {}
+    local rawItems = {}
+    
     if GetResourceState('ox_inventory') == 'started' then
-        for item, data in pairs(exports.ox_inventory:Items()) do
-            Items[#Items + 1] = {
-                item = item,
-                name = data.label,
-                description = data.description,
-                weight = data.weight
-            }
-        end
-    else
-        for item, data in pairs(QBCore.Shared.Items) do
-            Items[#Items + 1] = {
-                item = item,
-                name = data.label,
-                description = data.description,
-                weight = data.weight
-            }
-        end
+        rawItems = exports.ox_inventory:Items()
+    elseif QBCore and QBCore.Shared and QBCore.Shared.Items then
+        rawItems = QBCore.Shared.Items
     end
+
+    for item, data in pairs(rawItems) do
+        local itemName = item
+        local itemLabel = data.label
+        
+        if not itemLabel then
+            Debug(('[mri_Qadmin] ^3AVISO: Item "%s" está mal formatado ou sem Label!^7'):format(tostring(itemName)))
+            Debug(('[mri_Qadmin] Dados brutos do item "%s": %s'):format(tostring(itemName), json.encode(data)))
+            itemLabel = itemName or "Unknown"
+        end
+
+        Items[#Items + 1] = {
+            item = itemName,
+            name = itemLabel,
+            description = data.description or "",
+            weight = data.weight or 0
+        }
+    end
+
+    if #Items == 0 then
+        Debug('^1[mri_Qadmin] ERRO CRÍTICO: Nenhum item foi encontrado no Shared.Items ou ox_inventory!^7')
+    else
+        Debug(('[mri_Qadmin] Sucesso: %d itens carregados para a interface.'):format(#Items))
+    end
+    
     return Items
 end
 _G.GetItemsList = GetItemsList
